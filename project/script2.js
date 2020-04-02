ba3cbc30fbd0f4d91dd71827e0c4dea782358e68
vmrogova <vmrogova@gmail.com>
2020-04-03 01:23:24 +0300

    Перевод доконца, footer, все сообщения об ошибках, отредактирован код.

--- project/script2.js
+++ project/script2.js
@@ -1,6 +1,6 @@
 $("#sendMail").on("click", function(){
-  var email = $("#exampleInputEmail").val().trim();
-  var pass = $("#exampleInputPass").val().trim();
+  var email = $("#emailA").val().trim();
+  var pass = $("#password").val().trim();
 var data = new Object();
 data.username = email;
 data.userPassword = pass;
@@ -8,7 +8,7 @@ data = JSON.stringify(data);
   $.ajax({ type: "POST",
   url: "http://localhost:8080/user/authenticate",
    data: data,
-   headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } ,
+   headers: { 'Accept': 'application/json', 'Content-Type': 'application/json; charset=UTF-8' } ,
     success: function(data){
       alert(data);
     }
