9a4427ad4e8cebb7c359ffee450f21ec24b8c5b1
vmrogova <vmrogova@gmail.com>
2020-04-03 10:40:21 +0300

    Наброски авторизации

--- project/script.js
+++ project/script.js
@@ -2,10 +2,10 @@ var element = document.getElementById("events");
 for (let i=0; i < 6; i++){
 var div_card = document.createElement('div');
 if (element.className == "row justify-content-center eng"){
-div_card.innerHTML = "<div class='card-body'><h4 class='card-title'>Event "+(i+1)+"</h4> <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p><a href='#' class='btn btn-primary'>Take part</a></div>";
+div_card.innerHTML = "<div class='card-body'><h4 class='card-title'>Event "+(i+1)+"</h4> <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p><a href='' id='btn_regevent"+(i+1)+"' class='btn btn-primary "+(i+1)+"'>Take part</a></div>";
 }
 else{
-div_card.innerHTML = "<div class='card-body'><h4 class='card-title'>Мероприятие "+(i+1)+"</h4> <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p><a href='#' class='btn btn-primary'>Зарегаться</a></div>";
+div_card.innerHTML = "<div class='card-body'><h4 class='card-title'>Мероприятие "+(i+1)+"</h4> <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p><a href='' id='btn_regevent"+(i+1)+"' class='btn btn-primary "+(i+1)+"'>Зарегаться</a></div>";
 }
 div_card.className = "card col-md-4 col-lg-3 mb-3";
 element.appendChild(div_card);
@@ -17,6 +17,31 @@ option.value = i+1;
 option.innerHTML = i+1;
 element.appendChild(option);
 }
+if (localStorage.getItem("user_id")){
+  document.getElementById("aut").style="display : none";
+  document.getElementById("exit").style="display : inline";
+  var hello = document.getElementById("hello");
+  if (hello.className =="eng mr-lg-5")
+  {
+  hello.innerHTML="Hello, " + localStorage.getItem("user_id");
+}
+else{
+  hello.innerHTML="Здравствуйте, " + localStorage.getItem("user_id");
+}
+}
+else {
+  document.getElementById("aut").style="display : inline";
+  document.getElementById("exit").style="display : none";
+  var hello = document.getElementById("hello");
+  if (hello.className =="eng mr-lg-5")
+  {
+    hello.innerHTML="Log in.";
+  }
+  else{
+  hello.innerHTML="Авторизируйтесь для регистрации на мероприятие.";
+}
+}
+
 (function() {
   'use strict';
 
--- project/script2.js
+++ project/script2.js
@@ -1,8 +1,10 @@
+let user_id;
 $("#sendMail").on("click", function(){
   var email = $("#emailA").val().trim();
   var pass = $("#password").val().trim();
 var data = new Object();
 data.username = email;
+user_id = email;
 data.userPassword = pass;
 data = JSON.stringify(data);
   $.ajax({ type: "POST",
@@ -14,4 +16,20 @@ data = JSON.stringify(data);
     }
 
   });
+  localStorage.setItem("user_id", user_id);
 });
+$("#exit").on("click", function(){
+  localStorage.removeItem("user_id");
+  });
+for (let i=0; i < 6; i++){
+$("#btn_regevent"+(i+1)).on("click", function(){
+  if (!localStorage.getItem("user_id")){
+    alert("Авторизируйтесь")
+  }
+  else{
+  var element = document.getElementById("btn_regevent"+(i+1));
+  var id = element.className.slice(16);
+  alert(localStorage.getItem("user_id"));
+ }
+  });
+}
