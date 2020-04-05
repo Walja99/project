a44793a8f2f903590aa614f79e6aa9347cb8366a
vmrogova <vmrogova@gmail.com>
2020-04-05 14:31:21 +0300

    Создание мероприятия

--- project/script2.js
+++ project/script2.js
@@ -32,14 +32,14 @@ $("#crEvent").on("click", function(){
   var startTime = $("#startTime").val().trim();
   var endTime = $("#endTime").val().trim();
   var place = $("#place").val().trim();
-  var num = $("#num").val().trim();
+  var num = $("#choose").val().trim();
 var data = new Object();
 data.name = name;
 data.orgId = user_id;
 data.startTime = startTime;
 data.endTime = endTime;
 data.place = place;
-data.count = num;
+data.booths = num;
 data = JSON.stringify(data);
   $.ajax({ type: "POST",
   url: "http://localhost:8080/event/create",
