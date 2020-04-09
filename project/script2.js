ab1c930c7fea7b3c80c22ccddc5f08c7bb24b287
vmrogova <vmrogova@gmail.com>
2020-04-09 11:57:16 +0300

    исправление fetch

--- project/script2.js
+++ project/script2.js
@@ -74,9 +74,8 @@ data.phoneNumber = phoneNumber;
 data.organization = organization;
 data = JSON.stringify(data);
 
-  const response = await fetch(url, {
+   fetch("http://localhost:8080/create_user", {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    url: "http://localhost:8080/create_user",
     mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
