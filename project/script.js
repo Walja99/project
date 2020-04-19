67f22a21f36be2cd731c92287828693a024469e8
vmrogova <vmrogova@gmail.com>
2020-04-19 22:15:11 +0300

    Исправление запросов

--- project/script.js
+++ project/script.js
@@ -1,79 +1,122 @@
-$("#crTime").on("click", function(){
-  var adr="http://localhost:8080/booths/available";
-  var co;
-  var startTime = $("#startTime").val().trim();
-  var endTime = $("#endTime").val().trim();
-  var data = new Object();
-  data.startTime = startTime;
-  data.endTime = endTime;
-  data = JSON.stringify(data);
-  tok="Bearer"+localStorage.getItem("token");
-  $.ajax({
-    type: "GET",
-    url: adr,
-    data: data,
-    headers: { 'Accept': 'application/json',
-    'Content-Type': 'application/json; charset=UTF-8',
-    'X-Authorization': tok} ,
-    success: function(data2){
-      co = data2.count;
-    }
-});
-  var element = document.getElementById("choose");
-  for (let i=0; i < co; i++){
-    var option = document.createElement('option');
-    option.value = i+1;
-    option.innerHTML = i+1;
-    element.appendChild(option);}
-});
-
-(function createContent() {
-  var element = document.getElementById("events");
-  for (let i=0; i < 22; i++) {
-    var div_card = document.createElement('div');
-    if (element.className == "row justify-content-center eng") {
-      div_card.innerHTML = "<div class='card-body'><h4 class='card-title'>Event "+(i+1)+"</h4> <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p><a href='' id='btn_regevent"+(i+1)+"' class='btn btn-primary "+(i+1)+"'>Take part</a></div>";
-    } else {
-      div_card.innerHTML = "<div class='card-body'><h4 class='card-title'>Мероприятие "+(i+1)+"</h4> <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p><a href='' id='btn_regevent"+(i+1)+"' class='btn btn-primary "+(i+1)+"'>Зарегаться</a></div>";
-    }
-    div_card.className = "card col-md-4 col-lg-3 mb-3";
-    element.appendChild(div_card);
-  };
-
-  if (localStorage.getItem("user_id")) {
-    document.getElementById("aut").style="display : none";
-    document.getElementById("exit").style="display : inline";
-    var hello = document.getElementById("hello");
-    if (hello.className =="eng mr-lg-5") {
-      hello.innerHTML="Hello, " + localStorage.getItem("user_id");
-    } else {
-      hello.innerHTML="Здравствуйте, " + localStorage.getItem("user_id");
-    }
-  } else {
-    document.getElementById("aut").style="display : inline";
-    document.getElementById("exit").style="display : none";
-    var hello = document.getElementById("hello");
-    if (hello.className =="eng mr-lg-5") {
-      hello.innerHTML="Log in.";
-    } else {
-      hello.innerHTML="Авторизируйтесь для регистрации на мероприятие.";
-    }
-  };
-})();
-(function checkInput() {
-  'use strict';
-  window.addEventListener('load', function() {
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-  // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
+var eventsResponse;
+//Функция для получения кол-ва будок
+$("#crTime").on("click", function () {
+    var startTime = new Date($("#startTime").val().trim());
+    var endTime = new Date($("#endTime").val().trim());
+    var url = "http://localhost:8080/booths/available?from=" + startTime.getTime() + "&to=" + endTime.getTime();
+    tok = "Bearer " + localStorage.getItem("token")
+    fetch(url, {
+        method: 'GET', headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=UTF-8',
+            'X-Authorization': tok
+        },
+    }).then(results => results.json().then(size => {
+        console.log(size)
+        var element = document.getElementById("choose");
+        for (let i = 0; i < size; i++) {
+            var option = document.createElement('option');
+            option.value = i + 1;
+            option.innerHTML = i + 1;
+            element.appendChild(option);
         }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
+        }
+    ))
+});
+//Функция для ркгистрации на мероприятие
+function setOnRegClick() {
+    for (let i = 0; i < eventsResponse.length; i++) {
+        $("#btn_regevent" + (i + 1)).on("click", function () {
+            if (!localStorage.getItem("user_id")) {
+                alert("Авторизируйтесь")
+            } else {
+                var element = document.getElementById("btn_regevent" + (i + 1));
+                var id = element.className.slice(16);
+                var url = "http://localhost:8080/event/register_user?userId=" + localStorage.getItem("user_id") + "&meetupId=" + id;
+                tok = "Bearer " + localStorage.getItem("token")
+                fetch(url, {
+                    method: 'GET', headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json; charset=UTF-8'
+                    },
+                }).then(results => results.json()).then(console.log)
+            }
+        });
+    }
+}
+//Функция для создания карточек
+function showEvents() {
+    var element = document.getElementById("events");
+    for (let i = 0; i < eventsResponse.length; i++) {
+        var div_card = document.createElement('div');
+        if (element.className == "row justify-content-center eng") {
+            div_card.innerHTML =
+            "<div class='card-body'><h4 class='card-title'>" + eventsResponse[i]["name"] + "</h4>"+
+            " <p class='card-text'>Start: " + eventsResponse[i]["startTime"] + "\nEnd: " + eventsResponse[i]["endTime"] + "</p>" +
+            "<button  id='btn_regevent" + (i + 1) + "' class='btn btn-primary " + eventsResponse[i]["orgId"] + "'>Take part</button>"+
+            "</div>";
+        } else {
+            div_card.innerHTML =
+                "<div class='card-body'>" +
+                "<h4 class='card-title'>" + eventsResponse[i]["name"] + "</h4> " +
+                "<p class='card-text'>Начало: " + eventsResponse[i]["startTime"] + "\nКонец: " + eventsResponse[i]["endTime"] + "</p>" +
+                "<button id='btn_regevent" + (i + 1) + "' class='btn btn-primary " + eventsResponse[i]["orgId"] + "'>Зарегистрироваться</button>" +
+                "</div>";
+        }
+        div_card.className = "card col-md-4 col-lg-3 mb-3";
+        element.appendChild(div_card);
+    }
+    setOnRegClick()
+    if (localStorage.getItem("user_id")) {
+        document.getElementById("aut").style = "display : none";
+        document.getElementById("exit").style = "display : inline";
+        var hello = document.getElementById("hello");
+        if (hello.className == "eng mr-lg-5") {
+            hello.innerHTML = "Hello, " + localStorage.getItem("login");
+        } else {
+            hello.innerHTML = "Здравствуйте, " + localStorage.getItem("login");
+        }
+    } else {
+        document.getElementById("aut").style = "display : inline";
+        document.getElementById("exit").style = "display : none";
+        var hello = document.getElementById("hello");
+        if (hello.className == "eng mr-lg-5") {
+            hello.innerHTML = "Log in.";
+        } else {
+            hello.innerHTML = "Авторизируйтесь для регистрации на мероприятие.";
+        }
+    }
+}
+//Функция для создания контента
+(function createContent() {
+    var url = "http://localhost:8080/events";
+    fetch(url, {
+        method: 'GET', headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    }).then(response => {
+        response.json().then(json => {
+            eventsResponse = json
+            showEvents();
+        })
+    })
+})();
+//Функция для проверки данных
+(function checkInput() {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
 })();
--- project/script2.js
+++ project/script2.js
@@ -1,104 +1,86 @@
-$("#sendMail").on("click", function() {
-  var email = $("#emailA").val().trim();
-  var pass = $("#password").val().trim();
-  var data = new Object();
-  data.username = email;
-  data.userPassword = pass;
-  data = JSON.stringify(data);
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:8080/user/authenticate",
-    data: data,
-    headers: { 'Accept': 'application/json',
-    'Content-Type': 'application/json; charset=UTF-8' },
-    success: function(n) {
-      localStorage.setItem("user_id", n.id);
-      localStorage.setItem("login", n.login);
-      localStorage.setItem("token", n.token);
-    },
-    error: function(t) {
-      localStorage.setItem("user_id", "error");
-    }
-  });
-  alert("для отладки");
-});
-
-$("#crEvent").on("click", function(){
-  if (!localStorage.getItem("user_id")) {
-    alert("Aвторизируйтесь")
-  } else {
-    var name = $("#name").val().trim();
-    var startTime = $("#startTime").val().trim();
-    var endTime = $("#endTime").val().trim();
-    var place = $("#place").val().trim();
-    var num = $("#choose").val().trim();
-    var data2 = new Object();
-    data2.name = name;
-    data2.orgId = localStorage.getItem("user_id");
-    data2.startTime = startTime;
-    data2.endTime = endTime;
-    data2.place = place;
-    data2.booths = num;
-    data2 = JSON.stringify(data2);
-    tok="Bearer"+localStorage.getItem("token");
+//Функция авторизации
+$("#sendMail").on("click", function () {
+    var email = $("#emailA").val().trim();
+    var pass = $("#password").val().trim();
+    var data = new Object();
+    data.username = email;
+    data.userPassword = pass;
+    data = JSON.stringify(data);
     $.ajax({
-      type: "POST",
-      url: "http://localhost:8080/event/create",
-      data: data2,
-      headers: { 'Accept': 'application/json',
-      'Content-Type': 'application/json; charset=UTF-8',
-      'X-Authorization': tok},
+        type: "POST",
+        url: "http://localhost:8080/user/authenticate",
+        data: data,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        success: function (n) {
+            localStorage.setItem("user_id", n.id);
+            localStorage.setItem("login", n.firstName);
+            localStorage.setItem("token", n.token);
+        },
+        error: function (t) {
+            alert("Не удалось авторизоваться");
+        }
     });
-  }
 });
-
-$("#rege").on("click", function() {
-  var email =  $("#email").val().trim();
-  var firstName = $("#firstName").val().trim();
-  var password = $("#pass").val().trim();
-  var secondName = $("#lastName").val().trim();
-  var rank = "jjjjj";
-  var sex = "m";
-  var birth = $("#birthDate").val().trim();
-  var phoneNumber = $("#phone").val().trim();
-  var organization = $("#org").val().trim();
-  var data = new Object();
-  data.email = email;
-  data.password = password;
-  data.firstName = firstName;
-  data.secondName = secondName;
-  data.rank = rank;
-  data.birth = birth;
-  data.phoneNumber = phoneNumber;
-  data.organization = organization;
-  data = JSON.stringify(data);
-  fetch("http://localhost:8080/create_user", {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: data // body data type must match "Content-Type" header
-  });
-});
-
-$("#exit").on("click", function() {
-  localStorage.removeItem("user_id");
-});
-
-for (let i=0; i < 22; i++) {
-  $("#btn_regevent"+(i+1)).on("click", function() {
+//Функция создания мероприятия
+$("#crEvent").on("click", function () {
     if (!localStorage.getItem("user_id")) {
-      alert("Авторизируйтесь")
+        alert("Aвторизируйтесь")
     } else {
-      var element = document.getElementById("btn_regevent"+(i+1));
-      var id = element.className.slice(16);
-      alert(localStorage.getItem("user_id"));
+        var name = $("#name").val().trim();
+        var startTime = $("#startTime").val().trim();
+        var endTime = $("#endTime").val().trim();
+        var place = $("#place").val().trim();
+        var num = $("#choose").val().trim();
+        var data = {
+            "name": name,
+            "orgId": localStorage.getItem("user_id"),
+            "startTime": startTime,
+            "endTime": endTime,
+            "place": place,
+            "booths": num
+        }
+        tok = "Bearer " + localStorage.getItem("token")
+        fetch('http://localhost:8080/event/create', {
+            method: 'POST', headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=UTF-8',
+                'X-Authorization': tok
+            },
+            body: JSON.stringify(data)
+        }).then(results => results.json()).then(console.log("ppp"))
     }
-  });
-};
+});
+//Функция регистрации
+$("#rege").on("click", function () {
+    var email = $("#email").val().trim();
+    var firstName = $("#firstName").val().trim();
+    var password = $("#pass").val().trim();
+    var secondName = $("#lastName").val().trim();
+    var rank = "jjjjj";
+    var sex = "m";
+    var birth = $("#birthDate").val().trim();
+    var phoneNumber = $("#phone").val().trim();
+    var organization = $("#org").val().trim();
+    var data = {
+        "email": email,
+        "password": password,
+        "firstName": firstName,
+        "secondName": secondName,
+        "rank": rank,
+        "sex": sex,
+        "birth": birth,
+        "phoneNumber": phoneNumber,
+        "organization": organization
+    }
+    fetch('http://localhost:8080/create_user', {
+        method: 'POST',
+        body: JSON.stringify(data)
+    }).then(results => results.json()).then(r => alert("Регистрация прошла успешно"))
+});
+//Функция выход
+$("#exit").on("click", function () {
+    localStorage.removeItem("user_id")
+});
